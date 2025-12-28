7345f6efa203afc7c7c08850ca561bf8f4270dd7
Billiamin Abbas <abbasbilliamin2@gmail.com>
2025-12-28 10:07:07 +0000

    data ingestion script built with docker

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,11 +3,13 @@ import pandas as pd
 
 print("arguments", sys.argv)
 
-day = int(sys.argv[1])
-print(f"Running pipeline for day {day}")
+month = int(sys.argv[1])
+print(f"hello pipeline, month={month}")
 
 
-df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df = pd.DataFrame({"day": [1, 2], "no_pssngr": [3, 4]})
+
+df['month'] = month
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
